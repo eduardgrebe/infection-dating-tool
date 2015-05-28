0a23f4d3d6ce191fa9cc5d6211f48a5a5cf72050
Gareth Priede <gtp@implicitdesign.co.za>
2015-05-28 12:31:31 +0200

    added django_shell script

---
+++ scripts/django_shell.sh
@@ -0,0 +1,20 @@
+ #!/bin/bash
+
+ROOT=`git rev-parse --show-toplevel`
+cd ${ROOT}
+SRC=${ROOT}/cephia
+SITE_PATH=${SRC}
+VENV=${ROOT}/venv
+
+echo "activate virtualenv"
+cd ${VENV}
+. ./bin/activate
+if [ $? != 0 ]; then
+    echo "failed to activate virtualenv at ${VENV}: ABORTING"
+    exit 1
+fi
+cd -
+
+cd ${SITE_PATH}
+python manage.py shell --settings=cephia.management_settings
+
