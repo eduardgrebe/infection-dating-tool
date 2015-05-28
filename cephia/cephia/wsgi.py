87a19bdb17c5df8ae27bd61f43cefc75ac074ffe
Gareth Priede <gtp@implicitdesign.co.za>
2015-05-28 12:30:29 +0200

    updated for wsgi.py

--- cephia/cephia/wsgi.py
+++ cephia/cephia/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "cephia.settings"
+
+PROJECT_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..")
+os.environ["HOME"] = PROJECT_ROOT
+
+sys.stdout = sys.stderr
+sys.path.append(os.path.join(PROJECT_ROOT, "cephia"))
+sys.path.append(os.path.join(PROJECT_ROOT, "cephia", "cephia"))
+sys.path.append(os.path.join(PROJECT_ROOT, "venv","lib","python2.7","site-packages"))
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cephia.settings")
-
 application = get_wsgi_application()
