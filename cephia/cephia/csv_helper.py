8a9e2b0b98b2184daee62a0ff4a5161c5db63cfe
jarryd <jarryd@implicitdesign.co.za>
2015-07-16 14:42:28 +0200

    csv writer file name fix

--- cephia/cephia/csv_helper.py
+++ cephia/cephia/csv_helper.py
@@ -6,6 +6,8 @@ def _response_for_data_download(request, data, filename, mimetype=None):
         mimetype = 'application/octet-stream'
     if data is None:
         response = HttpResponse(content_type=mimetype)
+        content_disposition = 'attachment; filename=%s' % (filename)
+        response['Content-Disposition'] = content_disposition
     else:
         response = HttpResponse(data, content_type=mimetype)
         content_disposition = 'attachment; filename=%s' % (filename)
@@ -17,3 +19,4 @@ def get_csv_response(filename_to_show_in_download_box):
     response = _response_for_data_download(request=None, data=None,
                                            filename=filename_to_show_in_download_box, mimetype='text/csv')
     return response, csv.writer(response)
+
