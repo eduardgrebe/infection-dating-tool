fb7bfa9cc0268d23786446c995efba0877dd962f
Jarryd <jarryd@implicitdesign.co.za>
2015-08-14 17:09:00 +0200

    bug fix

--- cephia/cephia/migrations/0058_auto_20150814_1524.py
+++ cephia/cephia/migrations/0058_auto_20150814_1524.py
@@ -13,32 +13,32 @@ class Migration(migrations.Migration):
         source_study_list = [{'name':'SIPP',
                               'description':'Options study - SIPP samples (large volume collections)'},
                              {'name':'ARCHIVE',
-                              'description':'Options study - Archive samples (remnants from the old archive)'}
-                             {'name':'IAVI'
-                              'description':'IAVI study - from core lab in London'}
-                             {'name':'UCSD'
-                              'description':'UCSD study, San Diego, CA'}
-                             {'name':'SCOPE'
-                              'description':'SCOPE study, San Francisco, CA'}
-                             {'name':'AMPLIAR'
-                              'description':'AMPLIAR study, Brazil'}
-                             {'name':'SFMHS'
-                              'description':'San Francisco Men’s Health Study'}
-                             {'name':'CTS'
-                              'description':'Blood bank - ARC/MS (American Red Cross)'}
-                             {'name':'BCP'
-                              'description':'Blood bank - Blood Centers of the Pacific (BSRI)'}
-                             {'name':'BRAZIL'
-                              'description':'Blood bank - Brazil'}
-                             {'name':'SANBS'
-                              'description':'Blood bank  - SANBS'}
-                             {'name':'CAPRISA'
-                              'description':'CAPRISA study – South Africa'}
-                             {'name':'FPSHSP'
-                              'description':'Fundação Pro-Sangue-Hemocentro de São Paulo, Brazil'}
-                             {'name':'GAMA'
-                              'description':'CRESIB malaria cohort'}
-                             {'name':'IMPACTA'
+                              'description':'Options study - Archive samples (remnants from the old archive)'},
+                             {'name':'IAVI',
+                              'description':'IAVI study - from core lab in London'},
+                             {'name':'UCSD',
+                              'description':'UCSD study, San Diego, CA'},
+                             {'name':'SCOPE',
+                              'description':'SCOPE study, San Francisco, CA'},
+                             {'name':'AMPLIAR',
+                              'description':'AMPLIAR study, Brazil'},
+                             {'name':'SFMHS',
+                              'description':'San Francisco Men’s Health Study'},
+                             {'name':'CTS',
+                              'description':'Blood bank - ARC/MS (American Red Cross)'},
+                             {'name':'BCP',
+                              'description':'Blood bank - Blood Centers of the Pacific (BSRI)'},
+                             {'name':'BRAZIL',
+                              'description':'Blood bank - Brazil'},
+                             {'name':'SANBS',
+                              'description':'Blood bank  - SANBS'},
+                             {'name':'CAPRISA',
+                              'description':'CAPRISA study – South Africa'},
+                             {'name':'FPSHSP',
+                              'description':'Fundação Pro-Sangue-Hemocentro de São Paulo, Brazil'},
+                             {'name':'GAMA',
+                              'description':'CRESIB malaria cohort'},
+                             {'name':'IMPACTA',
                               'description':'Asociación Civil Impacta Saludy Educación'}]
 
         model.objects.all().delete()
