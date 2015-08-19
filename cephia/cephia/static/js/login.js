356a1abe8f1f93a3f76d8edf91d88f569a14f3f3
Akshar <akshar@implicitdesign.co.za>
2015-08-19 21:38:57 +0200

    *** issue112 Fix login page

---
+++ cephia/cephia/static/js/login.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.homeBtn').on('click', function() {
+        $('.nonBlur').toggleClass('blur');
+        $('.blur_login').slideToggle(500);
+    });
+    $("#id_username").attr('placeholder', 'Username');
+    $("#id_password").attr('placeholder', 'Password');
+    
+});
+
+
