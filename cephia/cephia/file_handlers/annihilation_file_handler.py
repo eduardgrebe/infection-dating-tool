91fcbd38933243503dcb6800518cec7c863f1f5a
Jarryd <jarryd@implicitdesign.co.za>
2015-08-26 09:46:19 +0200

    interim commit

--- cephia/cephia/file_handlers/annihilation_file_handler.py
+++ cephia/cephia/file_handlers/annihilation_file_handler.py
@@ -3,11 +3,11 @@ from file_handler import FileHandler
 
 class AnnihilationFileHandler(FileHandler):
 
-    def __init__(self, annihilation_file):
-        super(AnnihilationFileHandler, self).__init__()
-        self.annihilation_file = annihilation_file
-        self.excel_annihilation_file = ExcelHelper(f=annihilation_file.data_file.url)
-        self.annihilation_row = None
+    def __init__(self, aliquot_file):
+        super(AliquotFileHandler, self).__init__()
+        self.aliquot_file = aliquot_file
+        self.excel_aliquot_file = ExcelHelper(f=aliquot_file.data_file.url)
+        self.aliquot_row = None
 
         self.registered_columns = ['parent_label',
                                    'aliquot_label',
@@ -19,43 +19,39 @@ class AnnihilationFileHandler(FileHandler):
                                    'volume_units',
                                    'reason']
 
-        self.existing_columns = self.excel_annihilation_file.read_header()
+        self.existing_columns = self.excel_aliquot_file.read_header()
 
     def parse(self):
-        from models import AnnihilationRow
+        from models import AliquotRow
 
-        header = self.excel_annihilation_file.read_header()
+        header = self.excel_aliquot_file.read_header()
         rows_inserted = 0
         rows_failed = 0
 
-        for row_num in range(self.excel_annihilation_file.nrows):
+        for row_num in range(self.excel_aliquot_file.nrows):
             try:
                 if row_num >= 1:
-                    row = self.excel_annihilation_file.read_row(row_num)
+                    row = self.excel_aliquot_file.read_row(row_num)
                     row_dict = dict(zip(header, row))
-
-                    #this is to ignore blanks and can probably be done better
-                    if not row_dict['parent id']:
-                        continue
                     
-                    annihilation_row = AnnihilationRow.objects.create(parent_id=row_dict['parent id'],
-                                                                      child_id=row_dict['child id'],
-                                                                      child_volume=row_dict['child volume'],
-                                                                      number_of_aliquot=row_dict['number of aliquot'],
-                                                                      annihilation_date=row_dict['annihilation date'],
-                                                                      reason=row_dict['reason'],
-                                                                      panel_type=row_dict['panel type'],
-                                                                      panel_inclusion_criteria=row_dict['panel inclusion criteria'],
-                                                                      fileinfo=self.annihilation_file,
-                                                                      state='pending')
+                    aliquot_row = AliquotRow.objects.create(parent_label=row_dict['parent_label'],
+                                                            aliquot_label=row_dict['aliquot_label'],
+                                                            volume=row_dict['volume'],
+                                                            volume_units=row_dict['volume_units'],
+                                                            aliquoting_date_yyyy=row_dict['aliquoting_date_yyyy'],
+                                                            aliquoting_date_mm=row_dict['aliquoting_date_mm'],
+                                                            aliquoting_date_dd=row_dict['aliquoting_date_dd'],
+                                                            aliquot_reason=row_dict['reason'],
+                                                            fileinfo=self.aliquot_file,
+                                                            state='pending')
 
 
                     rows_inserted += 1
 
             except Exception, e:
                 logger.exception(e)
-                self.annihilation_file.message = "row " + str(row_num) + ": " + e.message
-                self.annihilation_file.save()
+                self.aliquot_file.message = "row " + str(row_num) + ": " + e.message
+                self.aliquot_file.save()
                 return 0, 1
 
         return rows_inserted, rows_failed
@@ -63,29 +59,24 @@ class AnnihilationFileHandler(FileHandler):
 
     def process(self):
         
-        from models import AnnihilationRow, Specimen, Reason, AliquotingReason, PanelInclusionCriteria
+        from models import AliquotRow, Specimen
         
         rows_inserted = 0
         rows_failed = 0
 
-        for annihilation_row in AnnihilationRow.objects.filter(fileinfo=self.annihilation_file, state__in=['pending']):
+        for aliquot_row in AliquotRow.objects.filter(fileinfo=self.aliquot_file, state__in=['pending']):
 
             try:
                 with transaction.atomic():
-
-                    reason, reason_created = Reason.objects.get_or_create(name=annihilation_row.reason)
-                    aliquoting_reason, aliquoting_reason_created = AliquotingReason.objects.get_or_create(name=annihilation_row.panel_type)
-                    panel_inclusion_criteria, panel_inclusion_criteria_created = PanelInclusionCriteria.objects.get_or_create(name=annihilation_row.panel_inclusion_criteria)
-
-                    if annihilation_row.parent_id == annihilation_row.child_id:
+                    if aliquot_row.parent_id == aliquot_row.child_id:
                         try:
-                            parent_specimen = Specimen.objects.get(specimen_label=annihilation_row.parent_id, parent_label=None)
+                            parent_specimen = Specimen.objects.get(specimen_label=aliquot_row.parent_id, parent_label=None)
                         except Specimen.DoesNotExist:
                             raise Exception("Specimen does not exist")
                             
-                        parent_specimen.num_containers = annihilation_row.number_of_aliquot
-                        parent_specimen.volume = annihilation_row.child_volume
-                        parent_specimen.modified_date = self.get_date(annihilation_row.annihilation_date)
+                        parent_specimen.num_containers = aliquot_row.number_of_aliquot
+                        parent_specimen.volume = aliquot_row.child_volume
+                        parent_specimen.modified_date = self.get_date(aliquot_row.aliquot_date)
                         parent_specimen.reason = reason
                         parent_specimen.aliquoting_reason = aliquoting_reason
                         parent_specimen.panel_inclusion_criteria = panel_inclusion_criteria
@@ -93,37 +84,35 @@ class AnnihilationFileHandler(FileHandler):
 
                     else:
                         try:
-                            parent_specimen = Specimen.objects.get(specimen_label=annihilation_row.parent_id, parent_label=None)
+                            parent_specimen = Specimen.objects.get(specimen_label=aliquot_row.parent_id, parent_label=None)
                         except Specimen.DoesNotExist:
                             raise Exception("Specimen does not exist")
 
-                        parent_specimen.modified_date = self.get_date(annihilation_row.annihilation_date)
+                        parent_specimen.modified_date = self.get_date(aliquot_row.aliquot_date)
                         parent_specimen.save()
 
-                        Specimen.objects.get_or_create(specimen_label=annihilation_row.child_id,
-                                                       parent_label=annihilation_row.parent_id,
-                                                       num_containers=annihilation_row.number_of_aliquot,
-                                                       volume=annihilation_row.child_volume,
+                        Specimen.objects.get_or_create(specimen_label=aliquot_row.child_id,
+                                                       parent_label=aliquot_row.parent_id,
+                                                       num_containers=aliquot_row.number_of_aliquot,
+                                                       volume=aliquot_row.child_volume,
                                                        spec_type=parent_specimen.spec_type,
                                                        reported_draw_date=parent_specimen.reported_draw_date,
                                                        source_study=parent_specimen.source_study,
-                                                       created_date=self.get_date(annihilation_row.annihilation_date),
+                                                       created_date=self.get_date(aliquot_row.aliquot_date),
                                                        reason=reason,
-                                                       aliquoting_reason=aliquoting_reason,
-                                                       panel_inclusion_criteria=panel_inclusion_criteria)
-
-                    annihilation_row.state = 'processed'
-                    annihilation_row.error_message = ''
-                    annihilation_row.date_processed = timezone.now()
-                    annihilation_row.save()
+                                                       aliquoting_reason=aliquoting_reason)
+                    aliquot_row.state = 'processed'
+                    aliquot_row.error_message = ''
+                    aliquot_row.date_processed = timezone.now()
+                    aliquot_row.save()
 
                     rows_inserted += 1
 
             except Exception, e:
                 logger.exception(e)
-                annihilation_row.state = 'error'
-                annihilation_row.error_message = e.message
-                annihilation_row.save()
+                aliquot_row.state = 'error'
+                aliquot_row.error_message = e.message
+                aliquot_row.save()
                 rows_failed += 1
                 continue
 
