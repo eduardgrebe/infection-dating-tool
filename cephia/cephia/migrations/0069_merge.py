67591cb96143dad186344ea0972823e4ed6eaeaa
Jarryd <jarryd@implicitdesign.co.za>
2015-09-01 10:49:18 +0200

    merge

---
+++ cephia/cephia/migrations/0069_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cephia', '0068_auto_20150831_1430'),
+        ('cephia', '0060_auto_20150826_1029'),
+    ]
+
+    operations = [
+    ]
