3acfe3753e236cb810c047792cfd83e300e04b9c
Jarryd <jarryd@implicitdesign.co.za>
2015-09-08 16:38:38 +0200

    interim commit

--- cephia/cephia/static/js/cephia.js
+++ cephia/cephia/static/js/cephia.js
@@ -1,43 +1,45 @@
 $(document).ready(function() {
-  //remove alerts after certain amount of time
-  $('.alert').fadeOut(4000);
+    //remove alerts after certain amount of time
+    $('.alert').fadeOut(4000);
+    $( ".datepicker" ).datepicker();
+    //initially disable upload file button
+    //$('.upload-btn').attr('disabled', 'disabled');
 
-  //initially disable upload file button
-  //$('.upload-btn').attr('disabled', 'disabled');
+    //enable and disable the upload file button
+    $('#id_data_file').on('change', function() {
+        if ($('#id_file_type').val() != "") {
+            $('.upload-btn').removeAttr('disabled');
+        }
+    });
 
-  //enable and disable the upload file button
-  $('#id_data_file').on('change', function() {
-    if ($('#id_file_type').val() != "") {
-      $('.upload-btn').removeAttr('disabled');
-    }
-  });
+    $('#id_file_type').on('change', function() {
+        if ($('#id_data_file').val() != "") {
+            $('.upload-btn').removeAttr('disabled');
+        }
+    });
+    //
 
-  $('#id_file_type').on('change', function() {
-    if ($('#id_data_file').val() != "") {
-      $('.upload-btn').removeAttr('disabled');
-    }
-  });
-  //
+    $('.btn-comment-modal').on('click', function(event) {
+        event.preventDefault();
+        var rowId = $(this).parent().data('row-id');
+        var fileId = $(this).parent().data('file-id');
+        var fileType = $(this).parent().data('file-type');
+        var url = "/row_comment/" + fileType + '/' + String(fileId) + '/' + String(rowId);
 
-  $('.btn-comment-modal').on('click', function(event) {
-    event.preventDefault();
-    var rowId = $(this).parent().data('row-id');
-    var fileId = $(this).parent().data('file-id');
-    var fileType = $(this).parent().data('file-type');
-    var url = "/row_comment/" + fileType + '/' + String(fileId) + '/' + String(rowId);
+        $.get(url, function(data, status) {
+            var response = JSON.parse(data);
+            if (status == "success") {
+                $(".comment-modal").html(response.response);
+                $( ".datepicker" ).datepicker();
+                $("#myModal").modal();
+            }
+        })
+    });
+    //
 
-    $.get(url, function(data, status) {
-      var response = JSON.parse(data);
-      if (status == "success") {
-        $(".comment-modal").html(response.response);
-        $( ".datepicker" ).datepicker();
-        $("#myModal").modal();
-      }
-    })
-  });
-  //
-
-  $( ".datepicker" ).datepicker();
+    $('.navtab').on('click', function(event) {
+        $(this).addClass('active');
+    });
 });
 
 
