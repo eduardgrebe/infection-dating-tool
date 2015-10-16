cba68a1ba18a35eb9add09bb8cecd03ee1f0b718
Jarryd <jarryd@implicitdesign.co.za>
2015-10-16 14:08:44 +0200

    14 day auto association

--- cephia/cephia/management/commands/associate_specimen_visit.py
+++ cephia/cephia/management/commands/associate_specimen_visit.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from cephia.models import Specimen, Visit
+from datetime import timedelta
+from django.db.models import Min
 import logging
 
 logger = logging.getLogger(__name__)
@@ -14,12 +16,26 @@ class Command(BaseCommand):
         for specimen in Specimen.objects.filter(visit=None, subject__isnull=False):
             try:
                 visit = Visit.objects.get(subject=specimen.subject, visit_date=specimen.reported_draw_date)
-                specimen.visit = visit
-                specimen.source_study = visit.source_study
-                specimen.visit_linkage = 'provisional'
-                specimen.save()
+
             except Visit.DoesNotExist:
-                continue
+                from_date = specimen.reported_draw_date - timedelta(days=14)
+                to_date = specimen.reported_draw_date + timedelta(days=14)
+                possible_visits = Visit.objects.filter(subject=specimen.subject,
+                                                       visit_date__gte=from_date,
+                                                       visit_date__lte=to_date)
+
+                if possible_visits.count() > 1:
+                    import pdb; pdb.set_trace()
+                    visit = possible_visits.aggregate(Min(visit_date))
+                elif possible_visits.count() == 0:
+                    continue
+                else:
+                    visit = possible_visits[0]
+
+            specimen.visit = visit
+            specimen.source_study = visit.source_study
+            specimen.visit_linkage = 'provisional'
+            specimen.save()
 
             num_associations = num_associations + 1
 
