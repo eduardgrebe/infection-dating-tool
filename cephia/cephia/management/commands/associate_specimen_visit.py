6faeb89ba88b71419a3a5a7f61c1023fe9aa9ab6
Jarryd <jarryd@implicitdesign.co.za>
2015-10-16 15:02:32 +0200

    roll back 14 day rule

--- cephia/cephia/management/commands/associate_specimen_visit.py
+++ cephia/cephia/management/commands/associate_specimen_visit.py
@@ -1,8 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from cephia.models import Specimen, Visit
-from datetime import timedelta
-from django.db.models import Min
 import logging
 
 logger = logging.getLogger(__name__)
@@ -16,26 +14,12 @@ class Command(BaseCommand):
         for specimen in Specimen.objects.filter(visit=None, subject__isnull=False):
             try:
                 visit = Visit.objects.get(subject=specimen.subject, visit_date=specimen.reported_draw_date)
-
+                specimen.visit = visit
+                specimen.source_study = visit.source_study
+                specimen.visit_linkage = 'provisional'
+                specimen.save()
             except Visit.DoesNotExist:
-                from_date = specimen.reported_draw_date - timedelta(days=14)
-                to_date = specimen.reported_draw_date + timedelta(days=14)
-                possible_visits = Visit.objects.filter(subject=specimen.subject,
-                                                       visit_date__gte=from_date,
-                                                       visit_date__lte=to_date)
-
-                if possible_visits.count() > 1:
-                    import pdb; pdb.set_trace()
-                    visit = possible_visits.aggregate(Min(visit_date))
-                elif possible_visits.count() == 0:
-                    continue
-                else:
-                    visit = possible_visits[0]
-
-            specimen.visit = visit
-            specimen.source_study = visit.source_study
-            specimen.visit_linkage = 'provisional'
-            specimen.save()
+                continue
 
             num_associations = num_associations + 1
 
