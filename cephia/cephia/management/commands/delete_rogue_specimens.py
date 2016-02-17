0de07867d12099eea2ca5037039c16467f8a77f7
Jarryd <jarryd@implicitdesign.co.za>
2016-02-17 16:52:09 +0200

    delete rogue specimen script

---
+++ cephia/cephia/management/commands/delete_rogue_specimens.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+from cephia.models import TransferInRow, Specimen
+from diagnostics.models import DiagnosticTestHistory
+from datetime import timedelta
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = 'Delete rogue specimens accidently reported'
+    args = '<type>'
+
+    def handle(self, *args, **options):
+        spec_file = open('BSRI_transferin_delete20160217_revised.csv')
+        headers = spec_file.readline()
+        for line in spec_file.readlines():
+            row_id = line.split(',')[0].rstrip()
+            try:
+                spec_row = TransferInRow.objects.get(pk=row_id)
+                if spec_row.specimen:
+                    spec_row.specimen.delete()
+                spec_row.delete()
+            except TransferInRow.DoesNotExist:
+                continue
+
