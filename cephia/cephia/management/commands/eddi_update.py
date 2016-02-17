4fcda4a846423252f3d2389e9c3d510f5c213523
Jarryd <jarryd@implicitdesign.co.za>
2016-02-17 11:08:55 +0200

    edsc date difference calculation fix

--- cephia/cephia/management/commands/eddi_update.py
+++ cephia/cephia/management/commands/eddi_update.py
@@ -22,37 +22,34 @@ class Command(BaseCommand):
                 self._handle_subject(subject_id)
 
     def _handle_subject(self, subject_id):
-            try:
-                tci_end = DiagnosticTestHistory.objects.filter(subject__id=subject_id, test_result='Positive', ignore=False).earliest('adjusted_date').adjusted_date
-            except DiagnosticTestHistory.DoesNotExist:
-                tci_end = None
+        edsc_days_diff = None
+        try:
+            tci_end = DiagnosticTestHistory.objects.filter(subject__id=subject_id, test_result='Positive', ignore=False).earliest('adjusted_date').adjusted_date
+        except DiagnosticTestHistory.DoesNotExist:
+            tci_end = None
                     
-            try:
-                tci_begin = DiagnosticTestHistory.objects.filter(subject__id=subject_id, test_result='Negative', ignore=False).latest('adjusted_date').adjusted_date
-            except DiagnosticTestHistory.DoesNotExist:
-                tci_begin = None
+        try:
+            tci_begin = DiagnosticTestHistory.objects.filter(subject__id=subject_id, test_result='Negative', ignore=False).latest('adjusted_date').adjusted_date
+        except DiagnosticTestHistory.DoesNotExist:
+            tci_begin = None
 
-            if tci_begin is None or tci_end is None:
-                eddi = None
-                tci_size = None
-            else:
-                eddi = tci_begin + timedelta(days=((tci_end - tci_begin).days / 2))
-                tci_size = abs((tci_end - tci_begin).days)
+        if tci_begin is None or tci_end is None:
+            eddi = None
+            tci_size = None
+        else:
+            eddi = tci_begin + timedelta(days=((tci_end - tci_begin).days / 2))
+            tci_size = abs((tci_end - tci_begin).days)
 
-                subject = Subject.objects.get(pk=subject_id)
-                if subject.edsc_reported and eddi:
-                    import pdb; pdb.set_trace()
-                    edsc_days_diff = timedelta(days=(eddi - tci_begin.subject.edsc_reported).days)
-                else:
-                    edsc_days_diff = None
+        subject = Subject.objects.get(pk=subject_id)
+        if subject.edsc_reported and eddi:
+            edsc_days_diff = timedelta(days=(eddi - subject.edsc_reported).days).days
                 
-                
-            subject_to_update = Subject.objects.get(pk=subject_id)
-            new_eddi = SubjectEDDI.objects.create(tci_begin=tci_begin,
-                                                  tci_end=tci_end,
-                                                  tci_size=tci_size,
-                                                  edsc_days_difference=edsc_days_diff,
-                                                  eddi=eddi)
+        subject_to_update = Subject.objects.get(pk=subject_id)
+        new_eddi = SubjectEDDI.objects.create(tci_begin=tci_begin,
+                                              tci_end=tci_end,
+                                              tci_size=tci_size,
+                                              edsc_days_difference=edsc_days_diff,
+                                              eddi=eddi)
 
-            subject_to_update.subject_eddi = new_eddi
-            subject_to_update.save()
+        subject_to_update.subject_eddi = new_eddi
+        subject_to_update.save()
