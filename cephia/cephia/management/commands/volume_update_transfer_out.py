7a99bb36b5f26fc079d61ee4386c5fe6a5f9b7c4
Jarryd Gerber <jarryd@implicitdesing.co.za>
2016-03-03 19:52:48 +0200

    quickfix

--- cephia/cephia/management/commands/volume_update_transfer_out.py
+++ cephia/cephia/management/commands/volume_update_transfer_out.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from cephia.models import Specimen, AliquotRow, HistoricalAliquotRow
+from cephia.models import Specimen, TransferOutRow, HistoricalTransferOutRow
 from django.db.models import F
 import logging
 
