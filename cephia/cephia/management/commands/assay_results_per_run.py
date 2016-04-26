e4b1043f6fd4c8d6527335cae332c9c688346cc2
Jarryd Gerber <jarryd@implicitdesign.co.za>
2016-04-26 15:46:59 +0200

    lag modification

--- cephia/cephia/management/commands/assay_results_per_run.py
+++ cephia/cephia/management/commands/assay_results_per_run.py
@@ -53,30 +53,43 @@ class Command(BaseCommand):
             self._handle_immunetics_newmixpeptide(assay_run)
 
     def _handle_lag_sedia(self, assay_run):
-        specimen_ids = LagSediaResult.objects.values_list('specimen', flat=True).filter(assay_run=assay_run).distinct()
-        for specimen_id in specimen_ids:
-            spec_results = LagSediaResult.objects.filter(assay_run=assay_run, specimen__id=specimen_id)
-            test_modes = [ spec.test_mode for spec in spec_results ]
-            lag_result = spec_results[0]
+        warning_msg = ''
+        specimen_ids = LagSediaResult.objects.values_list('specimen', flat=True)\
+                                             .filter(assay_run=assay_run)\
+                                             .exclude(test_mode='control').distinct()
 
-            if spec_results.count() == 1:
-                final_result = lag_result.ODn
-                method = 'singlet'
-            elif spec_results.count() > 1 and 'confirm_3' not in test_modes:
-                final_result = spec_results.aggregate(Sum('ODn'))['ODn__sum'] / spec_results.count()
-                method = 'mean_ODn_screen'
-            elif spec_results.count() > 1 and 'confirm_3' in test_modes:
-                confirm_results = sorted([ result.ODn for result in spec_results.filter(test_mode__startswith='confirm') ])
-                final_result = confirm_results[1]
-                method = 'median_of_confirms'
+        with transaction.atomic():
+            for specimen_id in specimen_ids:
+                spec_results = LagSediaResult.objects.filter(assay_run=assay_run, specimen__id=specimen_id)
+                test_modes = [ spec.test_mode for spec in spec_results ]
+                number_of_confirms = len([mode for mode in test_modes if "conf" in mode])
+                number_of_screens = len([mode for mode in test_modes if "screen" in mode])
+                lag_result = spec_results[0]
 
-            assay_result = AssayResult.objects.create(panel=assay_run.panel,
-                                                      assay=assay_run.assay,
-                                                      specimen=lag_result.specimen,
-                                                      assay_run=assay_run,
-                                                      test_date=lag_result.test_date,
-                                                      method=method,
-                                                      result=final_result)
+                if spec_results.count() == 1:
+                    final_result = lag_result.ODn
+                    method = 'singlet'
+                elif number_of_screens > 1 and number_of_confirms == 0:
+                    final_result = spec_results.aggregate(Sum('ODn'))['ODn__sum'] / spec_results.count()
+                    method = 'mean_ODn_screen'
+                elif number_of_confirms > 0:
+                    confirm_results = sorted([ result.ODn for result in spec_results.filter(test_mode__startswith='confirm') ])
+                    final_result = confirm_results[1]
+                    method = 'median_of_confirms'
+
+                if number_of_confirms != 2:
+                    warning_msg += "Unexpected number of 'confirm' records."
+                if number_of_screens == 0:
+                    warning_msg += "\nNo 'screen' records."
+
+                assay_result = AssayResult.objects.create(panel=assay_run.panel,
+                                                          assay=assay_run.assay,
+                                                          specimen=lag_result.specimen,
+                                                          assay_run=assay_run,
+                                                          test_date=lag_result.test_date,
+                                                          method=method,
+                                                          result=final_result,
+                                                          warning_msg=warning_msg)
 
     def _handle_lag_maxim(self, assay_run):
         pass
