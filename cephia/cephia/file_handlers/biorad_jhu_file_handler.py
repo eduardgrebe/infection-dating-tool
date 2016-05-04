f9d3859632bb75fcf1e2fa6e30f278ba8ad857c4
Jarryd Gerber <jarryd@implicitdesign.co.za>
2016-05-04 16:33:07 +0200

    biorad jhu fix

--- cephia/cephia/file_handlers/biorad_jhu_file_handler.py
+++ cephia/cephia/file_handlers/biorad_jhu_file_handler.py
@@ -88,18 +88,14 @@ class BioRadAvidityJHUFileHandler(FileHandler):
             try:
                 error_msg = ''
                 panel = Panel.objects.get(pk=panel_id)
-                panel_memberhsips = PanelMembership.objects.filter(panel=panel)
-                #assay = Assay.objects.get(name=self.assay_name)
 
                 try:
                     specimen = Specimen.objects.get(specimen_label=biorad_result_row.specimen_label,
                                                     specimen_type=panel.specimen_type,
                                                     parent_label__isnull=False)
                 except Specimen.DoesNotExist:
-                    error_msg += "Specimen not recognised.\n"
-
-                # if specimen.visit.id not in [ membership.id for membership in panel_memberhsips ]:
-                #     error_msg += "Specimen does not belong to any panel membership.\n"
+                    if biorad_result_row.specimen_purpose == 'panel_specimen':
+                        error_msg += "Specimen not recognised.\n"
 
                 if error_msg:
                     raise Exception(error_msg)
@@ -128,19 +124,31 @@ class BioRadAvidityJHUFileHandler(FileHandler):
 
     def process(self, panel_id, assay_run):
         from cephia.models import Specimen, Laboratory, Assay, Panel
-        from assay.models import BioRadAvidityJHUResultRow, BioRadAvidityJHUResult, AssayResult
+        from assay.models import BioRadAvidityJHUResultRow, BioRadAvidityJHUResult, AssayResult, PanelMembership
 
         rows_inserted = 0
         rows_failed = 0
 
+        assay = Assay.objects.get(name=self.assay_name)
+        panel = Panel.objects.get(pk=panel_id)
+        panel_memberhsips = PanelMembership.objects.filter(panel=panel)
+        panel_memberhsip_ids = [ membership.id for membership in panel_memberhsips ]
+
         for biorad_result_row in BioRadAvidityJHUResultRow.objects.filter(fileinfo=self.upload_file, state='validated'):
             try:
+                warning_msg = ''
+
                 with transaction.atomic():
-                    assay = Assay.objects.get(name=self.assay_name)
-                    panel = Panel.objects.get(pk=panel_id)
-                    specimen = Specimen.objects.get(specimen_label=biorad_result_row.specimen_label,
-                                                    specimen_type=panel.specimen_type,
-                                                    parent_label__isnull=False)
+                    specimen = None
+                    try:
+                        specimen = Specimen.objects.get(specimen_label=biorad_result_row.specimen_label,
+                                                        specimen_type=panel.specimen_type,
+                                                        parent_label__isnull=False)
+                    except Specimen.DoesNotExist:
+                        warning_msg += "Specimen not recognised.\n"
+
+                    # if specimen.visit.id not in panel_memberhsip_ids:
+                    #     warning_msg += "Specimen does not belong to any panel membership.\n"
 
                     biorad_result = BioRadAvidityJHUResult.objects.create(specimen=specimen,
                                                                           assay=assay,
@@ -153,10 +161,10 @@ class BioRadAvidityJHUFileHandler(FileHandler):
                                                                           well_untreated_dilwashsoln=biorad_result_row.well_untreated_dilwashsoln,
                                                                           well_treated_DEA=biorad_result_row.well_treated_DEA,
                                                                           specimen_purpose=biorad_result_row.specimen_purpose,
-                                                                          treated_DEA_OD=biorad_result_row.treated_DEA_OD,
-                                                                          untreated_dilwashsoln_OD=biorad_result_row.untreated_dilwashsoln_OD,
-                                                                          AI_reported=biorad_result_row.AI_reported,
-                                                                          AI=biorad_result_row.AI,
+                                                                          treated_DEA_OD=biorad_result_row.treated_DEA_OD or None,
+                                                                          untreated_dilwashsoln_OD=biorad_result_row.untreated_dilwashsoln_OD or None,
+                                                                          AI_reported=biorad_result_row.AI_reported or None,
+                                                                          AI=biorad_result_row.AI or None,
                                                                           assay_run=assay_run)
 
                     biorad_result_row.state = 'processed'
