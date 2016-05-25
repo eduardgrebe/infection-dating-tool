22889fda1ac2f9f367d4208842256feab60c9d4c
Jarryd Gerber <jarryd@implicitdesign.co.za>
2016-05-25 11:51:40 +0200

    temp commit

--- cephia/cephia/management/commands/parent_child_inheritance.py
+++ cephia/cephia/management/commands/parent_child_inheritance.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from cephia.models import Specimen, Subject
 from django.db.models import Q
+from django.db import transaction
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,23 +13,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         num_inherited = 0
 
-        for specimen in Specimen.objects.filter(Q(subject__isnull=True) | Q(visit__isnull=True), parent__isnull=False):
-            try:
-                parent_specimen = specimen.parent
-                
-                specimen.specimen_type = parent_specimen.specimen_type
-                specimen.reported_draw_date=parent_specimen.reported_draw_date
-                specimen.transfer_in_date=parent_specimen.transfer_in_date
-                specimen.parent=parent_specimen
-                specimen.visit=parent_specimen.visit
-                specimen.subject=parent_specimen.subject
-                specimen.source_study=parent_specimen.source_study
+        with transaction.atomic():
+            for specimen in Specimen.objects.filter(parent__isnull=False):
+                try:
+                    parent_specimen = specimen.parent
+                    specimen.specimen_type = parent_specimen.specimen_type
+                    specimen.reported_draw_date=parent_specimen.reported_draw_date
+                    specimen.transfer_in_date=parent_specimen.transfer_in_date
+                    specimen.parent=parent_specimen
+                    specimen.visit=parent_specimen.visit
+                    specimen.subject=parent_specimen.subject
+                    specimen.source_study=parent_specimen.source_study
 
-                specimen.save()
+                    specimen.save()
 
-            except Specimen.DoesNotExist:
-                continue
+                except Specimen.DoesNotExist:
+                    continue
 
-            num_inherited += 1
+                num_inherited += 1
 
         logger.info('Successfully associated "%s" visits with subjects' % num_inherited)
