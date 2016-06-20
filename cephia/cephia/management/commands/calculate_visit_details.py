b974f6ddbfb3c2de608668c762adf4d6a819a19c
Keith Grootboom <keith@implicitdesign.co.za>
2016-06-20 15:44:08 +0200

    catch update visit detail exception

--- cephia/cephia/management/commands/calculate_visit_details.py
+++ cephia/cephia/management/commands/calculate_visit_details.py
@@ -5,6 +5,7 @@ from django.db import transaction
 import logging
 
 logger = logging.getLogger(__name__)
+import traceback
 
 class Command(BaseCommand):
     help = 'Calculate visit details for each visit'
@@ -12,4 +13,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with transaction.atomic():
             for visit in Visit.objects.select_related('subject'):
-                visit.update_visit_detail()
+                try:
+                    visit.update_visit_detail()
+                except Exception:
+                    logger.error('Error with creating detail for visit: %s' % visit.pk)
+                    traceback.print_exc()
+                    continue
