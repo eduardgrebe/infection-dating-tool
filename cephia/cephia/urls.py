a3572c54a39a7c4e2532ec440a5d1fc7d52ecbe2
Keith Grootboom <keith@implicitdesign.co.za>
2016-06-22 17:12:41 +0200

    Removed admin pages. Changed ordering of report links

--- cephia/cephia/urls.py
+++ cephia/cephia/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 import views
 import reporting
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -9,7 +8,6 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
     url(r'^$', views.home, name='home'),
     url(r'^tms/$', views.table_management, name='table_management'),
