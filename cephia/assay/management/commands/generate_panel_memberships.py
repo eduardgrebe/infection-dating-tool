d0a2e965b286e938eb495ec96f284780ea7e79ea
Keith Grootboom <keith@implicitdesign.co.za>
2016-06-28 13:48:15 +0200

    Delete results for panel before recreating

--- cephia/assay/management/commands/generate_panel_memberships.py
+++ cephia/assay/management/commands/generate_panel_memberships.py
@@ -5,6 +5,7 @@ from assay.models import PanelMembership, Panel, AssayRun, AssayResult
 from assay import assay_result_factory
 import logging
 from collections import defaultdict
+from django.db import transaction
 
 logger = logging.getLogger(__name__)
 
@@ -24,13 +25,16 @@ class Command(BaseCommand):
             visit = result.specimen.visit
             visit_totals[visit.pk] += 1
 
-        for visit, visit_count in visit_totals.iteritems():
-            PanelMembership.objects.create(
-                visit_id=visit,
-                replicates=visit_count,
-                panel=assay_run.panel,
-                category='',
-                panel_inclusion_criterion=''
-            )
+        with transaction.atomic():
+            PanelMembership.objects.filter(panel=assay_run.panel).delete()
+
+            for visit, visit_count in visit_totals.iteritems():
+                PanelMembership.objects.create(
+                    visit_id=visit,
+                    replicates=visit_count,
+                    panel=assay_run.panel,
+                    category='',
+                    panel_inclusion_criterion=''
+                )
                 
 
