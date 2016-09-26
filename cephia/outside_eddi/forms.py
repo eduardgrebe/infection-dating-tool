5173dcd6ae0bfebe383a3f033e3551ee71422b2f
Keith <keith@implicitdesign.co.za>
2016-09-26 10:44:41 +0200

    Review changes

---
+++ cephia/outside_eddi/forms.py
@@ -0,0 +1,14 @@
+from user_management.form import UserCreationForm
+from django.contrib.auth.models import Group
+
+class EddiUserCreationForm(UserCreationForm):
+    
+    def save(self, commit=True):
+        
+        user = super(EddiUserCreationForm, self).save(True)
+        user.set_password(self.cleaned_data['password'])
+        user.is_active = True
+        user.save()
+        outside_eddi_group = Group.objects.get(name='Outside Eddi Users')
+        outside_eddi_group.user_set.add(user)
+        return user
--- cephia/user_management/forms.py
+++ cephia/user_management/forms.py
@@ -160,14 +160,3 @@ class UserCreationForm(ModelForm):
             )
         return verify_password
 
-class EddiUserCreationForm(UserCreationForm):
-    
-    def save(self, commit=True):
-        
-        user = super(EddiUserCreationForm, self).save(True)
-        user.set_password(self.cleaned_data['password'])
-        user.is_active = True
-        user.save()
-        outside_eddi_group = Group.objects.get(name='Outside Eddi Users')
-        outside_eddi_group.user_set.add(user)
-        return user
