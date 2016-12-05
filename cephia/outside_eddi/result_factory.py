03e28565d2e300fd49cf140cfa6cb7622e0925b7
Andrew Powrie <andrew@impd.co.za>
2016-12-05 17:32:44 +0200

    added result factory

---
+++ cephia/outside_eddi/result_factory.py
@@ -0,0 +1,29 @@
+from itertools import chain
+from django.conf import settings
+
+class ResultDownload(object):
+
+    def __init__(self, results):
+        
+        self.headers = ['Subject', 'EP DDI', 'LP DDI', 'Interval Size', 'EDDI']
+        self.content = []
+        self.results = results
+        self.prepare_content()
+
+    def get_content(self):
+        return self.content
+
+    def get_headers(self):
+        return self.headers
+
+    def getattr_or_none(self, obj, attr):
+        value = obj
+        value = getattr(value, attr, None)
+
+        return value
+
+    def prepare_content(self):
+        columns = ['subject_label', 'ep_ddi', 'lp_ddi', 'interval_size', 'eddi']
+        for result in self.results:
+            row = [ self.getattr_or_none(result, c) for c in columns ]
+            self.content.append(row)
