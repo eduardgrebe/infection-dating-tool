6edea1883f78a93865af1b7ad3fba157b8aa3a5c
Andrew Powrie <andrew@impd.co.za>
2016-12-15 17:33:26 +0200

    saving work on check_volumes

--- cephia/cephia/management/commands/check_volumes.py
+++ cephia/cephia/management/commands/check_volumes.py
@@ -5,6 +5,8 @@ from cephia.file_handlers.handler_imports import *
 from cephia.models import TransferInRow, Specimen, SpecimenType
 from django.db import connection
 import logging
+import csv
+from django.db.models.loading import get_model
 
 logger = logging.getLogger(__name__)
 
@@ -47,3 +49,38 @@ class Command(BaseCommand):
                 errors[spec['id']] = error_msgs
                 print errors[spec['id']]
 
+
+    def dump(qs, outfile_path):
+	"""
+	Takes in a Django queryset and spits out a CSV file.
+	
+	Usage::
+	
+		>> from utils import dump2csv
+		>> from dummy_app.models import *
+		>> qs = DummyModel.objects.all()
+		>> dump2csv.dump(qs, './data/dump.csv')
+	
+	Based on a snippet by zbyte64::
+		
+		http://www.djangosnippets.org/snippets/790/
+	
+	"""
+        model = qs.model
+	writer = csv.writer(open(outfile_path, 'w'))
+	
+	headers = []
+	for field in model._meta.fields:
+		headers.append(field.name)
+	writer.writerow(headers)
+	
+	for obj in qs:
+		row = []
+		for field in headers:
+			val = getattr(obj, field)
+			if callable(val):
+				val = val()
+			if type(val) == unicode:
+				val = val.encode("utf-8")
+			row.append(val)
+		writer.writerow(row)
