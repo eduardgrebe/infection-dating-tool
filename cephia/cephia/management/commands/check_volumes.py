d7fec95a8b2e038f6b8cf8badd586c88d7bdf15c
Andrew Powrie <andrew@impd.co.za>
2016-12-22 17:03:04 +0200

    made some updates to the check_volumes management command file

--- cephia/cephia/management/commands/check_volumes.py
+++ cephia/cephia/management/commands/check_volumes.py
@@ -17,26 +17,36 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         specimens = Specimen.objects.all()
-        spec_label_type = specimens.values('id', 'specimen_label', 'specimen_type__spec_type', 'volume', 'number_of_containers').distinct()
+        spec_label_type = specimens.values('id', 'specimen_label', 'specimen_type', 'specimen_type__spec_type', 'volume', 'number_of_containers').distinct()
 
         errors = {}
 
         for spec in spec_label_type:
             rows = TransferInRow.objects.filter(specimen_label=spec['specimen_label'], specimen_type=spec['specimen_type__spec_type'])
-            row_details = rows.values('id', 'number_of_containers', 'volume', 'state', 'error_message')
+            row_details = rows.values('id', 'number_of_containers', 'volume', 'state', 'error_message', 'specimen_type')
 
             error_msgs = {}
             volume_count = 0
             container_count = 0
-            
+
             for row in row_details:
-                containers = int(row['number_of_containers'])
-                volume = float(row['volume']) * containers
+                try:
+                    row_containers = int(row['number_of_containers'])
+                except TypeError:
+                    row_containers = 0
+                try:
+                    row_volume = float(row['volume'])
+                except TypeError:
+                    row_volume = 0
+
+                volume = row_volume * row_containers
                 volume_count = volume_count + volume
-                container_count = container_count + containers
+                container_count = container_count + row_containers
 
                 if row['state'] == 'error':
-                    error_msgs['Below the line error msg'] = 'Below the line id: %s, Error message: %s' % (row['id'], row['error_message'])
+                    if 'Below the line error msg' not in error_msgs:
+                        error_msgs['Below the line error msg'] = []
+                    error_msgs['Below the line error msg'].append('Below the line id: %s, Error message: %s' % (row['id'], row['error_message']))
 
 
             try:
@@ -47,16 +57,35 @@ class Command(BaseCommand):
                 spec_number_of_containers = int(spec['number_of_containers'])
             except TypeError:
                 spec_number_of_containers = 'None'
-            
+
             if spec_volume != volume_count:
                 error_msgs['Specimen volume'] = spec_volume
-                error_msgs['Below the Lines total volume'] = volume_count or 'None'
+                
+                if row_details:
+                    error_msgs['Below the Lines total volume'] = volume_count or 'None'
+                    error_msgs['Below the Lines volumes'] = [ x['volume'] or 'None' for x in row_details ]
+                else:
+                    error_msgs['Info'] = 'no transfer in rows found for specimen label: %s, specimen_type: %s' % (
+                        spec['specimen_label'],
+                        spec['specimen_type__spec_type']
+                    )
+                    
             if spec_number_of_containers != container_count:
                 error_msgs['Specimen containers'] = spec_number_of_containers
-                error_msgs['Below the Lines total containers'] = container_count or 'None'
+                
+                if row_details:
+                    error_msgs['Below the Lines total containers'] = container_count or 'None'
+                    error_msgs['Below the Lines containers'] = [ x['number_of_containers'] or 'None' for x in row_details ]
+                else:
+                    error_msgs['Info'] = 'no transfer in rows found for specimen label: %s, specimen_type: %s' % (
+                        spec['specimen_label'],
+                        spec['specimen_type__spec_type']
+                    )
 
             if error_msgs:
                 error_msgs['Specimen label'] = spec['specimen_label']
+                error_msgs['Specimen type'] = spec['specimen_type__spec_type']
+                error_msgs['Below the line specimen types'] = [ x['specimen_type'] for x in row_details ]
                 errors[spec['id']] = error_msgs
 
         dump(errors, outfile_path)
@@ -65,7 +94,7 @@ class Command(BaseCommand):
 def dump(errors, outfile_path):
     writer = csv.writer(open(outfile_path, 'w'))
 
-    headers = ['Specimen id', 'Specimen label', 'Below the line error msg', 'Specimen volume', 'Below the Lines total volume', 'Specimen containers', 'Below the Lines total containers']
+    headers = ['Specimen id', 'Specimen label', 'Below the line specimen types', 'Specimen type', 'Below the line error msg', 'Specimen volume', 'Below the Lines total volume', 'Below the Lines volumes', 'Specimen containers', 'Below the Lines total containers', 'Below the Lines containers', 'Info']
     writer.writerow(headers)
 
     for error in errors:
