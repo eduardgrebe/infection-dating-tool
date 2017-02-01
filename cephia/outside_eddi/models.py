b778fe315351c7d1e7a960953d3c5877dcde2636
Andrew Powrie <andrew@impd.co.za>
2017-02-01 17:28:38 +0200

    renaming model names, etc

--- cephia/outside_eddi/models.py
+++ cephia/outside_eddi/models.py
@@ -16,32 +16,26 @@ from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 from django.db.models import Q
 
-class OutsideEddiDiagnosticTestHistory(models.Model):
+class IDTDiagnosticTestHistory(models.Model):
     class Meta:
-        db_table = "outside_eddi_diagnostic_test_history"
+        db_table = "idt_diagnostic_test_history"
 
-    history = HistoricalRecords()
-    subject = ProtectedForeignKey('OutsideEddiSubject', null=True, blank=False, related_name='outside_eddi_test_history')
-    data_file = ProtectedForeignKey('OutsideEddiFileInfo', null=False, blank=False, related_name='test_history')
+    subject = ProtectedForeignKey('IDTSubject', null=True, blank=False, related_name='outside_eddi_test_history')
+    data_file = ProtectedForeignKey('IDTFileInfo', null=False, blank=False, related_name='test_history')
     test_code = models.CharField(max_length=255, null=True, blank=True)
-    # test = ProtectedForeignKey('OutsideEddiDiagnosticTest', null=True, blank=False)
     test_date = models.DateField(null=True, blank=False)
     adjusted_date = models.DateField(null=True, blank=False)
     test_result = models.CharField(max_length=15, null=True, blank=False)
-    ignore = models.BooleanField(blank=False, default=False)
 
     def __unicode__(self):
         return u'%s - %s' % (self.test_date, self.test_result)
 
 
-class Study(models.Model):
+class IDTDiagnosticTest(models.Model):
     class Meta:
-        unique_together = ("name", "user")
+        db_table = "idt_diagnostic_tests"
+        unique_together = ('name', 'user')
 
-    name = models.CharField(max_length=50)
-    user = ProtectedForeignKey('cephia.CephiaUser')
-
-class OutsideEddiDiagnosticTest(models.Model):
     CATEGORIES = (
         ('1st_gen_lab', '1st Gen Lab Assay (Viral Lysate IgG sensitive Antibody)'),
         ('2nd_gen_lab', '2nd Gen Lab Assay (Recombinant IgG sensitive Antibody)'),
@@ -56,15 +50,10 @@ class OutsideEddiDiagnosticTest(models.Model):
         ('viral_load', 'Viral Load'),
         ('western_blot', 'Western blot'),
     )
-    
+
     name = models.CharField(max_length=255, null=False, blank=False)
     user = ProtectedForeignKey('cephia.CephiaUser', null=True, blank=True)
     category = models.CharField(choices=CATEGORIES, max_length=255, null=True, blank=True)
-    history = HistoricalRecords()
-
-    class Meta:
-        db_table = "outside_eddi_diagnostic_tests"
-        unique_together = ('name', 'user')
 
     def __str__(self):
         return '%s' % (self.name)
@@ -75,24 +64,23 @@ class OutsideEddiDiagnosticTest(models.Model):
         return default_property
 
 
-class OutsideEddiTestPropertyEstimateQuerySet(QuerySet):
+class IDTTestPropertyEstimateQuerySet(QuerySet):
     def for_user(self, user):
         return self.filter(models.Q(user=user) | models.Q(user=None))
 
-class OutsideEddiTestPropertyEstimate(models.Model):
+class IDTTestPropertyEstimate(models.Model):
     class Meta:
-        db_table = "outside_eddi_test_property_estimates"
+        db_table = "idt_test_property_estimates"
         
-    objects = OutsideEddiTestPropertyEstimateQuerySet.as_manager()
+    objects = IDTTestPropertyEstimateQuerySet.as_manager()
     
-    active_property = models.BooleanField(blank=False, default=False)
-    is_default = models.BooleanField(blank=False, default=False)
-    test = models.ForeignKey(OutsideEddiDiagnosticTest, null=False, blank=True, related_name='properties')
+    user_default = models.BooleanField(blank=False, default=False)
+    global_default = models.BooleanField(blank=False, default=False)
+    test = models.ForeignKey(IDTDiagnosticTest, null=False, blank=True, related_name='properties')
     user = ProtectedForeignKey('cephia.CephiaUser', null=True, blank=True)
 
     estimate_label = models.CharField(max_length=255, null=False, blank=True)
     comment = models.CharField(max_length=255, null=False, blank=True)
-
 
     diagnostic_delay = models.FloatField(null=True, blank=False)
     diagnostic_delay_mean = models.FloatField(null=True, blank=False)
@@ -107,23 +95,24 @@ class OutsideEddiTestPropertyEstimate(models.Model):
     diagnostic_delay_range = models.CharField(max_length=255, null=True, blank=True)
     diagnostic_delay_iqr = models.CharField(max_length=255, null=True, blank=True)
 
-    history = HistoricalRecords()
-
     def __str__(self):
         return '%s' % (self.id)
 
 
 class TestPropertyMapping(models.Model):
-
-    code = models.CharField(max_length=255)
-    test = ProtectedForeignKey('OutsideEddiDiagnosticTest', null=True, blank=True)
-    test_property = ProtectedForeignKey('OutsideEddiTestPropertyEstimate', null=True, blank=True)
-    user = ProtectedForeignKey('cephia.CephiaUser')
-
     class Meta:
+        db_table = "idt_test_property_mapping"
         unique_together = ('code', 'user')
 
-class OutsideEddiFileInfo(models.Model):
+    code = models.CharField(max_length=255)
+    test = ProtectedForeignKey('IDTDiagnosticTest', null=True, blank=True)
+    test_property = ProtectedForeignKey('IDTTestPropertyEstimate', null=True, blank=True)
+    user = ProtectedForeignKey('cephia.CephiaUser')
+    
+
+class IDTFileInfo(models.Model):
+    class Meta:
+        db_table = "idt_file_info"
 
     STATE_CHOICES = (
         ('pending','Pending'),
@@ -140,8 +129,6 @@ class OutsideEddiFileInfo(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     state = models.CharField(choices=STATE_CHOICES, max_length=15, null=False, blank=False, default='pending')
     message = models.TextField(blank=True)
-    history = HistoricalRecords()
-    task_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
     deleted = models.BooleanField(default=False)
 
     def __unicode__(self):
@@ -154,7 +141,7 @@ class OutsideEddiFileInfo(models.Model):
         return self.filename().split('.')[-1]
 
     def create_mapping(self, user):
-        test_names = list(OutsideEddiDiagnosticTest.objects.filter(
+        test_names = list(IDTDiagnosticTest.objects.filter(
             Q(user=user) | Q(user=None)
         ).values_list('name', flat=True))
         
@@ -165,7 +152,7 @@ class OutsideEddiFileInfo(models.Model):
             if TestPropertyMapping.objects.filter(code=code, user=user).exists():
                 mapping = TestPropertyMapping.objects.get(code=code, user=user)
             elif code in test_names:
-                test = OutsideEddiDiagnosticTest.objects.get(name=code)
+                test = IDTDiagnosticTest.objects.get(name=code)
                 test_property = test.get_default_property()
             
                 mapping = TestPropertyMapping.objects.create(
@@ -184,11 +171,13 @@ class OutsideEddiFileInfo(models.Model):
         return file_maps
 
     
-class OutsideEddiSubject(models.Model):
+class IDTSubject(models.Model):
+    class Meta:
+        db_table = "idt_subjects"
+        unique_together = ("subject_label", "user")
     
     subject_label = models.CharField(max_length=255, null=True, blank=True, db_index=True)
     user = ProtectedForeignKey('cephia.CephiaUser', null=True, blank=True, related_name='subjects')
-    history = HistoricalRecords()
     edsc_reported  = models.DateField(null=True, blank=True, default=None)
 
     ep_ddi = models.DateField(null=True, blank=True)
@@ -196,13 +185,9 @@ class OutsideEddiSubject(models.Model):
     interval_size = models.IntegerField(null=True, blank=True)
     edsc_days_difference = models.IntegerField(null=True, blank=True)
     eddi = models.DateField(null=True, blank=True)
-    recalculate = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.subject_label
-
-    class Meta:
-        unique_together = ("subject_label", "user")
 
     def calculate_eddi(self, user, data_file, lp_ddi, ep_ddi):
         edsc_days_diff = None
