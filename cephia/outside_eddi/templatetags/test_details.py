61717ba16b8362e7d685bad63398d5b3af7e67a4
Andrew Powrie <andrew@impd.co.za>
2017-02-02 12:56:06 +0200

    saving changes

--- cephia/outside_eddi/templatetags/test_details.py
+++ cephia/outside_eddi/templatetags/test_details.py
@@ -1,13 +1,13 @@
-from django.template import Library
-from outside_eddi.models import OutsideEddiDiagnosticTest
+# from django.template import Library
+# from outside_eddi.models import OutsideEddiDiagnosticTest
 
-register = Library()
+# register = Library()
 
-@register.simple_tag
-def test_details(test_id):
-    details = ''
-    if test_id:
-        test = OutsideEddiDiagnosticTest.objects.filter(id = test_id).first()
-        details = test.description
-    return details
+# @register.simple_tag
+# def test_details(test_id):
+#     details = ''
+#     if test_id:
+#         test = OutsideEddiDiagnosticTest.objects.filter(id = test_id).first()
+#         details = test.description
+#     return details
 
