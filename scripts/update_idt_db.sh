ac3d153aae4ff3f2e6e7348052547bd411ccabd5
Andrew Powrie <andrew@impd.co.za>
2017-02-07 11:26:37 +0200

    trying to get test update working on deploy

--- scripts/update_idt_db.sh
+++ scripts/update_idt_db.sh
@@ -20,7 +20,7 @@ cd -
 
 echo "Running management command update_cephia_infection_dating_tool_tests_and_properties"
 cd ${SITE_PATH}
-python manage.py update_cephia_infection_dating_tool_tests_and_properties  --noinput --verbosity=0 --settings=cephia.management_settings
+python manage.py update_cephia_infection_dating_tool_tests_and_properties
 cd -
 
 echo "Done"
