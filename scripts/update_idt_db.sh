dc5015bbd18c8194c8482119ce4414ac98e3c586
Andrew Powrie <andrew@impd.co.za>
2017-02-07 11:23:05 +0200

    trying to get test update working on deploy

--- scripts/update_idt_db.sh
+++ scripts/update_idt_db.sh
@@ -8,6 +8,15 @@ cd ${ROOT}
 SRC=${ROOT}/cephia
 SITE_PATH=${SRC}
 
+echo "activate virtualenv"
+cd ${VENV}
+. ./bin/activate
+if [ $? != 0 ]; then
+    echo "failed to activate virtualenv at ${VENV}: ABORTING"
+    exit 1
+fi
+cd -
+
 echo "Running management command update_cephia_infection_dating_tool_tests_and_properties"
 cd ${SITE_PATH}
 python manage.py update_cephia_infection_dating_tool_tests_and_properties  --noinput --verbosity=0 --settings=cephia.management_settings
