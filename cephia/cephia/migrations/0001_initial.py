501e49dad15f4d2229a7c78bc5b317f310851a48
Andrew Powrie <andrew@impd.co.za>
2017-04-25 19:40:24 +0200

    fixed some html text colours

--- cephia/cephia/migrations/0001_initial.py
+++ cephia/cephia/migrations/0001_initial.py
@@ -17,19 +17,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # migrations.RunSQL(
-        #     'ALTER DATABASE cephia CHARACTER SET utf8 COLLATE utf8_bin;'
-        # ),
-        # migrations.RunSQL(
-        #     "SELECT CONCAT('ALTER TABLE ',  table_name, ' CHARACTER SET utf8 COLLATE utf8_bin;') FROM information_schema.TABLES AS T, information_schema.`COLLATION_CHARACTER_SET_APPLICABILITY` AS C WHERE C.collation_name = T.table_collation AND T.table_schema = 'cephia' AND ( C.CHARACTER_SET_NAME != 'utf8' OR C.COLLATION_NAME != 'utf8_bin' );"
-        # ),
-        # migrations.RunSQL(
-        #     "SELECT CONCAT('ALTER TABLE `', table_name, '` MODIFY `', column_name, '` ', DATA_TYPE, '(', CHARACTER_MAXIMUM_LENGTH, ') CHARACTER SET UTF8 COLLATE utf8_bin', (CASE WHEN IS_NULLABLE = 'NO' THEN ' NOT NULL' ELSE '' END), ';') FROM information_schema.COLUMNS  WHERE TABLE_SCHEMA = 'cephia' AND DATA_TYPE = 'varchar'  AND ( CHARACTER_SET_NAME != 'utf8' OR COLLATION_NAME != 'utf8_bin' );"
-        # ),
-        # migrations.RunSQL(
-        #     "SELECT CONCAT('ALTER TABLE `', table_name, '` MODIFY `', column_name, '` ', DATA_TYPE, ' CHARACTER SET UTF8 COLLATE utf8_bin', (CASE WHEN IS_NULLABLE = 'NO' THEN ' NOT NULL' ELSE '' END), ';') FROM information_schema.COLUMNS  WHERE TABLE_SCHEMA = 'cephia' AND DATA_TYPE != 'varchar' AND ( CHARACTER_SET_NAME != 'utf8' OR COLLATION_NAME != 'utf8_bin' );"
-        # ),
-        
         migrations.CreateModel(
             name='CephiaUser',
             fields=[
@@ -61,15 +48,3 @@ class Migration(migrations.Migration):
             ],
         ),
     ]
-
-
-
-
-# Dealing with Foreign Key Constraints
-# It may be necessary to ignore foreign key constraints when making changes to a large number of columns. You can use the SET FOREIGN_KEY_CHECKS command to ignore foreign key constraints while you update the database.
-
-# SET FOREIGN_KEY_CHECKS=0;
- 
-# -- Insert your other SQL Queries here...
- 
-# SET FOREIGN_KEY_CHECKS=1;
