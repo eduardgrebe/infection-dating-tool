a05dade8743203573df03471174fcbc6e8127482
Andrew Powrie <andrew@impd.co.za>
2017-07-03 14:10:29 +0200

    trying a reverse loop through values rather than reversing the index so that the x and y values can still be calculated correctly

--- cephia/infection_dating_tool/graph_image_generator.py
+++ cephia/infection_dating_tool/graph_image_generator.py
@@ -1,28 +1,41 @@
 import numpy as np
 import numpy.random
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def heat_map_graph(x_value, y_value):
-
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111)
     x_step = x_value*2.0/64
     xedges = np.arange(0, x_value*2+x_step, x_step)
     y_step = y_value*2.0/64
     yedges = np.arange(y_value/2, y_value*2+y_step, y_step)
 
     heatmap = np.zeros(shape=(len(yedges),len(xedges)))
-    for x_count, x in enumerate(xedges):
-        for y_count, y in enumerate(yedges):
-            heatmap[len(yedges)-y_count-1][len(xedges)-x_count-1] = float(y)*x
+    # for x_count, x in enumerate(xedges):
+    #     for y_count, y in enumerate(yedges):
+    #         heatmap[len(yedges)-y_count-1][len(xedges)-x_count-1] = float(y)*x
 
-    
+    for x_count, x in enumerate(reversed(xedges)):
+        for y_count, y in enumerate(reversed(yedges)):
+            heatmap[y_count][x_count] = float(y)*x
+
     heatmap = np.flipud(np.transpose(heatmap))
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
-    plt.clf()
+    # ax.clf()
     plt.title('Residual risk')
     plt.xlabel('Incidence in donar population')
     plt.ylabel('Window of residual risk')
-    plt.imshow(heatmap, extent=extent)
+    
+    im = ax.imshow(heatmap, extent=extent)
+    ax.set_aspect('auto')
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="2.5%", pad=0.3)
+    cb = plt.colorbar(im,cax=cax)
+    # cb.set_label('')
+
     plt.show()
 
     return plt
