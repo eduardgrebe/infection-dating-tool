b8788829f1b147a62311104df2c7e912641161be
Andrew Powrie <andrew@impd.co.za>
2017-07-06 15:11:53 +0200

    saving work on graph image generator from meeting

--- cephia/infection_dating_tool/graph_image_generator.py
+++ cephia/infection_dating_tool/graph_image_generator.py
@@ -6,27 +6,30 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-def heat_map_graph(x_value, y_value):
+def heat_map_graph(x_value, y_value, number_of_donations=None):
     fig = plt.figure(figsize=(10,10))
     ax = fig.add_subplot(111)
     x_step = x_value*2.0/64
     xedges = np.arange(0, x_value*2+x_step, x_step)
     y_step = y_value*2.0/64
-    yedges = np.arange(y_value/2, y_value*2+y_step, y_step)
+    yedges = np.arange(0, y_value*2+y_step, y_step)
 
     heatmap = np.zeros(shape=(len(yedges),len(xedges)))
 
     for x_count, x in enumerate(xedges):
         for y_count, y in enumerate(yedges):
             # (window/365)*(incidence) # risk per donation in %
-            heatmap[y_count][x_count] = float(y/365)*x
+            # (float(y/365)*x)/100 * num_donations
+            if not number_of_donations:
+                heatmap[y_count][x_count] = float(y/365)*x
+            else:
+                heatmap[y_count][x_count] = (float(y/365)*x)/100 * number_of_donations
     
     heatmap = np.flipud(np.transpose(heatmap))
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
     # extent = [-3, 3, -3, 3]
 
     # ax.clf()
-    plt.title('Residual risk per donation (incidence vs infectious window)')
     plt.xlabel('Incidence in donar population (% p.a.)')
     plt.ylabel('Window of residual risk (days)')
     
@@ -36,7 +39,12 @@ def heat_map_graph(x_value, y_value):
     divider = make_axes_locatable(ax)
     # cax = divider.append_axes("right", size="2.5%", pad=0.3)
     cb = plt.colorbar(im)
-    cb.set_label('Probability per donation (%)')
+    if not number_of_donations:
+        plt.title('Residual risk per donation (incidence x infectious window)')
+        cb.set_label('Probability per donation (%)')
+    else:
+        plt.title('Expected number of infectious donations (incidence x infectious window)')
+        cb.set_label('Expected number of infectious donations')
 
     plt.show()
 
