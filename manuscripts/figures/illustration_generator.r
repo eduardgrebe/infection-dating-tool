8703eccc0896abc325a63fe52604d6e455b76f0b
Eduard Grebe <eduardgrebe@sun.ac.za>
2018-02-16 13:50:56 +0200

    Totally broken code with start of refactoring

--- manuscripts/figures/illustration_generator.r
+++ manuscripts/figures/illustration_generator.r
@@ -1,3 +1,11 @@
+if(Sys.info()['login']=='eduardgrebe') {
+  setwd("~/dev/infection-dating-tool/manuscripts/figures/")
+} else if(Sys.info()['login']=='eduardgrebe') {
+  setwd(".")
+} else {
+  setwd(".")
+}
+
 #Tool for generating plots that illustrate the value of the knowing the details of individual HIV detectability
 #Definitions
 
@@ -10,54 +18,63 @@
 # Note: generate_family function don't return the time axis - this should be defined somewhere more central in the script.
 
   ##parameters are as follows
-#slope is lambda
-#scale is k (exponent of exponent)
+#scale is lambda
+#shape is k (exponent of exponent)
 #center_position is the position of the half-likelihood for an individual curve
 #mean_center_position is the mean position of half likelihood for a family of curves
 #sd_size is the standard deviation size of the normal distribution which is used to generate the half-likelihood positions
 #timeaxis is a vector of t-axis values. Should be defined centrally then passed to all functions
 
 
-generate_positions_for_individual_curves = function(n,slope,scale,mean_center_position,sd_size){
-  shift_to_half_likelihood = slope*(-1*log(1/2))^(1/scale)
+generate_positions_for_individual_curves = function(n,scale,shape,mean_center_position,sd_size){
+  shift_to_half_likelihood = scale*(-1*log(1/2))^(1/shape)
   myseq <- seq(1/(2*n),1-1/(2*n),1/n)
   positions <- qnorm(myseq,mean=mean_center_position-shift_to_half_likelihood,sd=sd_size)
   return(positions)
 }
 
 #negative curves
-individual_negative_curve = function(x,slope,scale,position){
-  return(ifelse(x-position<0,0,1-exp(-((x-position)/slope)^scale))) #will definitely not test negative if fDDT is before position = center position minus shift
+individual_negative_curve = function(x,scale,shape,position){
+  #ifelse(x-position<0,0,1-exp(-((x-position)/shape)^shape))
+  return(ifelse(x-position<0,0,1-exp(-((x-position)/scale)^shape))) #will definitely not test negative if fDDT is before position = center position minus shift
 }
-generate_individual_negative_curve = function(slope,scale,position,timeaxis){
-  #slope is lambda
-  #scale is k (exponent of exponent)
+generate_individual_negative_curve = function(scale,shape,position,timeaxis){
+  #scale is lambda
+  #shape is k (exponent of exponent)
   #center_position is the position of the half-likelihood
   #timeaxis is a vector of t-axis values. Suggest creating one centrally then passing to all functions
-  return(individual_negative_curve(timeaxis,slope,scale,position))
+  return(individual_negative_curve(timeaxis,scale,shape,position))
 }
 
-generate_family_negative_curves = function(n,slope,scale,mean_center_position,sd_size,timeaxis){
-  list_of_positions = generate_positions_for_individual_curves(n=n, slope=slope, scale=scale, mean_center_position=mean_center_position, sd_size=sd_size)
+generate_family_negative_curves = function(n,scale,shape,mean_center_position,sd_size,timeaxis){
+  list_of_positions = generate_positions_for_individual_curves(n=n, scale=scale, shape=shape, mean_center_position=mean_center_position, sd_size=sd_size)
   set_of_negative_curves = matrix(nrow=length(timeaxis),ncol=n)
   for (i in seq(1,n)){
-    set_of_negative_curves[,i]=generate_individual_negative_curve(slope=slope, scale=scale, position = list_of_positions[i], timeaxis=timeaxis)
+    set_of_negative_curves[,i]=generate_individual_negative_curve(scale=scale, shape=shape, position = list_of_positions[i], timeaxis=timeaxis)
   }
   return(set_of_negative_curves)
 }
 #positive curves
-individual_positive_curve = function(x,slope,scale,position){
-  return(ifelse(x-position<0,1,exp(-((x-position)/slope)^scale))) #definitely test positive if fDDT is before position = center position minus shift
-}
-generate_individual_positive_curve = function(slope,scale,position,timeaxis){
-  return(individual_positive_curve(timeaxis,slope,scale,position))
+individual_positive_curve = function(x,scale,shape,position){
+  #ifelse(x-position<0,1,exp(-((x-position)/shape)^shape))
+  return(ifelse(x-position<0,1,exp(-((x-position)/scale)^shape))) #definitely test positive if fDDT is before position = center position minus shift
 }
 
-generate_family_positive_curves = function(n,slope,scale, mean_center_position,sd_size,timeaxis){
-  list_of_positions = generate_positions_for_individual_curves(n=n, slope=slope, scale=scale, mean_center_position = mean_center_position, sd_size=sd_size)
+
+
+
+
+
+
+generate_individual_positive_curve = function(scale,shape,position,timeaxis){
+  return(individual_positive_curve(timeaxis,scale,shape,position))
+}
+
+generate_family_positive_curves = function(n,scale,shape, mean_center_position,sd_size,timeaxis){
+  list_of_positions = generate_positions_for_individual_curves(n=n, scale=scale, shape=shape, mean_center_position = mean_center_position, sd_size=sd_size)
   set_of_positive_curves = matrix(nrow=length(timeaxis),ncol=n)
   for (i in seq(1,n)){
-    set_of_positive_curves[,i] = generate_individual_positive_curve(slope=slope, scale=scale, position=list_of_positions[i], timeaxis=timeaxis)
+    set_of_positive_curves[,i] = generate_individual_positive_curve(scale=scale, shape=shape, position=list_of_positions[i], timeaxis=timeaxis)
   }
   return(set_of_positive_curves)
   
@@ -75,16 +92,18 @@ generate_product_curve = function(curve1,curve2){ #this doesn't need to be its o
 
 #given a curve and find the likelihood for all times the combined test results.
 likelihood_by_DDI = function(set_of_positive_curves,set_of_negative_curves,timeaxis){
+  if(ncol(set_of_negative_curves) != ncol(set_of_positive_curves)) {stop ("Huh?? Why are there different numbers of curves?")}
+  if(nrow(set_of_negative_curves) != nrow(set_of_positive_curves)) {stop ("Huh?? Why are there different numbers of time steps?")}
   likelihoods_per_time = rep(0,length(timeaxis))
   for (tpos in seq(1:length(timeaxis))){
-    cumu_likely=0
-    for (person in seq(1:ncol(set_of_negative_curves))){ #chronological order makes a trivial difference - there is 
+    cumu_likely <- 0
+    for (curvenumber in seq(1:ncol(set_of_negative_curves))){ #chronological order makes a trivial difference - there is 
                                                           #just a product per person
-      likelihood_forward = set_of_positive_curves[tpos,person]*set_of_negative_curves[tpos,person]
-      likelihood_backward = "The same thing unless I don't actually understand this task"
+      likelihood_forward <- set_of_positive_curves[tpos,curvenumber]*set_of_negative_curves[tpos,curvenumber]
+      likelihood_backward <- "The same thing unless I don't actually understand this task"
       cumu_likely <- cumu_likely + likelihood_forward
     }
-    likelihoods_per_time[tpos]=cumu_likely
+    likelihoods_per_time[tpos] <- cumu_likely / ncol(set_of_negative_curves)
   }
   return(likelihoods_per_time)
 }
@@ -126,23 +145,72 @@ plot_individual_time_likelihood = function(n,timeaxis,set_of_positive_curves,set
 }
 
 
+# New rough draft
+
+position_params_normal <- function() {
+  # generate_positions ... 
+}
+
+neg_positions <- position_params_normal(n, mean_delay = 15, sd_delay = 10)
+pos_positions <- position_params_normal(n, mean_delay = 10, sd_delay = 7) 
+
+test1 <- c(sensitivity_form = "weibull", fixed_params = c(scale = 5, shape = 5), 
+              variable_param = "position", variable_param_values = neg_positions)
+
+test2 <- c(f = weibull, fixed_params = c(scale = 5, shape = 5),
+              variable_param = "position", variable_param_values = neg_positions)
+              
+weibull <- function(t, params) {
+  return(ifelse(t-params$position<0,1,exp(-((t-params$position)/params$scale)^params$shape)))
+}
+
+generate_ind_lh <- function(test, time, result) {
+    browser()
+    with(test, {
+    lh <- f(t = , params = )
+    
+    return(lh_matrix)  
+  })
+}
+
+
+              
+generate_ind_likelihoods <- function(test_pos, test_neg, time_pos, time_neg) {
+  
+  return(pos_matrix, neg_matrix)
+}
+
+[pos,neg] <- 
+
+
 # script
-n=15
+n=50
+
+mean_diagnostic_delay_postive
+mean_diagnostic_delay_negative
+
+time_neg
+time_pos
+
+
+
 mean_center_position_negative_curves=45
 mean_center_position_positive_curves=55
 sd_size_positive = 15
-sd_size_negative = sd_size_positive
+sd_size_negative = 
+  sd_size_positive
 detail = 10
 timeaxis = seq(0,100,1/detail)
-slope = 1
-scale = 5
+
+scale = 5 # shape
+shape = 5 # shape
 
 illustration_timestep_size = 2
 illustration_timestart = 20
 illustration_number_timesteps = 10
 
-plotdata_negative = generate_family_negative_curves(n=n, slope=slope, scale=scale, mean_center_position=mean_center_position_negative_curves, sd_size= sd_size_negative, timeaxis = timeaxis)
-plotdata_positive = generate_family_positive_curves(n=n, slope=slope, scale=scale, mean_center_position=mean_center_position_positive_curves, sd_size= sd_size_positive, timeaxis = timeaxis)
+plotdata_negative = generate_family_negative_curves(n=n, scale=scale, shape=shape, mean_center_position=mean_center_position_negative_curves, sd_size= sd_size_negative, timeaxis = timeaxis)
+plotdata_positive = generate_family_positive_curves(n=n, scale=scale, shape=shape, mean_center_position=mean_center_position_positive_curves, sd_size= sd_size_positive, timeaxis = timeaxis)
 
 plot(timeaxis,plotdata_negative[,1],type='l',xlim=c(timeaxis[1],timeaxis[length(timeaxis)]),ylim=c(0,1.2),xlab="t",ylab="Likelihood",col='green')
 #points(plotdata[,1],plotdata[,3])
@@ -155,8 +223,8 @@ for (i in seq(1:n)){
 }
 positive_mean_naive <- rowMeans(plotdata_positive[,])
 negative_mean_naive <- rowMeans(plotdata_negative[,])
-product_of_means_naive <- positive_mean_naive*negative_mean_naive
-real_likelihood <- 1/n*likelihood_by_DDI(timeaxis = timeaxis, set_of_positive_curves = plotdata_positive, set_of_negative_curves = plotdata_negative)
+product_of_means_naive <- positive_mean_naive*negative_mean_naive # naive product of individual likelihoods
+real_likelihood <- likelihood_by_DDI(timeaxis = timeaxis, set_of_positive_curves = plotdata_positive, set_of_negative_curves = plotdata_negative)
 difference <- product_of_means_naive - real_likelihood
 print("The maximum difference for this scenario between the true likelihood and the totally naive approximation is on the next line:")
 print(max(difference))
@@ -168,7 +236,7 @@ lines(timeaxis,real_likelihood,lwd=1,col='purple')
 
 plot_individual_time_likelihood(n=n,timeaxis=timeaxis,time=illustration_timestart,set_of_positive_curves=plotdata_positive,set_of_negative_curves=plotdata_negative)
 
-jpeg("C:/Users/JumpCo Vostro3700/Pictures/code_for_discordant_test/frame_time_%03d.jpg")
+jpeg("frame_time_%03d.jpg")
 for (time in seq(illustration_timestart,illustration_timestart + illustration_number_timesteps*illustration_timestep_size,illustration_timestep_size)){
   plot_individual_time_likelihood(n=n, timeaxis=timeaxis,time=time,set_of_positive_curves = plotdata_positive,set_of_negative_curves = plotdata_negative)
 }
@@ -204,15 +272,15 @@ dev.off()
 #return(family_positive_likelihoods[,n]*family_negative_likelihoods[,n])
 #}
 
-##given a time, find where in the timescale it fits
-#calculate_time_index = function(timescale, time){
-#  return(which(abs(timescale-time) == min(abs(timescale-time))))
+##given a time, find where in the timeshape it fits
+#calculate_time_index = function(timeshape, time){
+#  return(which(abs(timeshape-time) == min(abs(timeshape-time))))
   
 #}
 
 ##given a time, calculate the likelihood that one person would get the two test results
-#calculate_total_likelihood_for_nth_person = function(timescale,time, n, family_negative_likelihoods, family_positive_likelihoods){
-#  time_index = calculate_time_index(timescale=timescale, time=time)
+#calculate_total_likelihood_for_nth_person = function(timeshape,time, n, family_negative_likelihoods, family_positive_likelihoods){
+#  time_index = calculate_time_index(timeshape=timeshape, time=time)
 #  return(family_negative_likelihoods[time_index,n]*family_positive_likelihoods[time_index,n])
 #}
 
