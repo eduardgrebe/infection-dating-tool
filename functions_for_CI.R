3c70944cea18b0563e1df5692dffc0389542dd12
Eduard Grebe <eduardgrebe@sun.ac.za>
2018-11-20 17:14:26 +0200

    Minor updates to functions for performance

--- functions_for_CI.R
+++ functions_for_CI.R
@@ -1,8 +1,8 @@
-f_left <- function(t, shape, delta) {
-  return((1 - (1-exp(shape*t))/(1-exp(-shape*delta)))/2)
+f_left <- function(t, scale, delta) {
+  return((1 - (1-exp(scale*t))/(1-exp(-scale*delta)))/2)
 }
 
-f <- function(t, t_centre=0, shape, delta) {
+f <- function(t, t_centre=0, scale, delta) {
   t <- t-t_centre
   rv <- 
     ifelse(t < -delta, 
@@ -10,97 +10,154 @@ f <- function(t, t_centre=0, shape, delta) {
            ifelse(t > delta,
                   1,
                   ifelse(t<=0,
-                         f_left(t,shape,delta),
-                         1-f_left(-t,shape,delta)))
+                         f_left(t,scale,delta),
+                         1-f_left(-t,scale,delta)))
            )
      
   return(rv)
 }
 
-g <-function(t, t_centre, shape, delta) {
-  f(t_centre-t, shape = shape, delta = delta)
+g <-function(t, t_centre, scale, delta) {
+  f(t_centre-t, scale = scale, delta = delta)
 }
 
-sigma_tree <- function(shape, sigma, d) {
+sigma_tree <- function(scale, sigma, d) {
   delta <- min(3*sigma,d)
   var <- sigma^2
-  (2+(-2-shape*delta*(2+shape*delta))*exp(-shape*delta)) / ( shape^2 * (1 - exp(-shape*delta))) - var
+  (2+(-2-scale*delta*(2+scale*delta))*exp(-scale*delta)) / ( scale^2 * (1 - exp(-scale*delta))) - var
 }
 
-likelihood <- function(t, t1 = 0, t2, shape1, delta1, shape2, delta2) {
-  f(t = t, t_centre = t1, shape = shape1, delta = delta1) * g(t = t, t_centre = t2, shape = shape2, delta = delta2)
+likelihood <- function(t, t1 = 0, t2, scale1, delta1, scale2, delta2) {
+  f(t = t, t_centre = t1, scale = scale1, delta = delta1) * g(t = t, t_centre = t2, scale = scale2, delta = delta2)
 }
+
+
+# testing
+d <- 15
+sigma <- 0.5
+
+find_scale_delta <- function(d, sigma) {
+  if ((delta <- 3*sigma) < d) {
+    #delta <- 3*sigma
+    scale <- 1.195554/sigma  
+  } else {
+    delta <- d
+    scale <- uniroot(f = sigma_tree, lower = 1/(10*sigma), upper = 10*(1/sigma), sigma = sigma, d = d)$root
+  }
+  return(c(delta = delta, scale = scale))
+}
+
+scale_delta <- find_scale_delta(d = 15, sigma =1)
+
 
 
 t1 <- 0
 d1 <- 15
-sigma1 <- 0.2*d1
+sigma1 <- 2
 delta1 <- min(3*sigma1,d1)
-shape1 <- uniroot(f = sigma_tree, lower = 1/(10*sigma), upper = 5*(1/sigma), sigma = sigma1, d = d1)$root
+scale1 <- uniroot(f = sigma_tree, lower = 1/(10*sigma1), upper = 5*(1/sigma1), sigma = sigma1, d = d1)$root
 
-t2 <- 30
-d2 <- 10
-sigma2 <- 0.3*d2
+t2 <- 25
+d2 <- 13
+sigma2 <- 2
 delta2 <- min(3*sigma2,d2)
-shape2 <- uniroot(f = sigma_tree, lower = 1/(10*sigma), upper = 5*(1/sigma), sigma = sigma2, d = d2)$root
+scale2 <- uniroot(f = sigma_tree, lower = 1/(10*sigma2), upper = 5*(1/sigma2), sigma = sigma2, d = d2)$root
 
-plot(x = seq(-15,45,0.1), 
-     y = likelihood(t = seq(-15,45,0.1), t1 = 0, t2 = 30, 
-                    shape1 = shape1, delta1 = delta1, 
-                    shape2 = shape2, delta2 = delta2), type = "l")
+plot(x = seq(-20,50,0.1), 
+     y = likelihood(t = seq(-20,50,0.1), t1 = t1, t2 = t2, 
+                    scale1 = scale1, delta1 = delta1, 
+                    scale2 = scale2, delta2 = delta2), type = "l")
 
-posterior_density_prop <- function(t, t1, t2, shape1, delta1, shape2, delta2, debug = F) {
-  if (debug) {browser()}
-  const <- cubature::adaptIntegrate(f = likelihood,
-                                    lowerLimit = t1-delta1,
-                                    upperLimit = t2+delta2,
-                                    t1 = t1,
-                                    t2 = t2,
-                                    shape1 = shape1,
-                                    delta1 = delta1,
-                                    shape2 = shape2,
-                                    delta2 = delta2)$integral
-  #like <- likelihood(t = t, t1=t1, t2=t2, shape1=shape1, delta1=delta1, shape2 = shape2, delta2=delta2)
+
+
+
+const <- cubature::adaptIntegrate(f = likelihood,
+                                  lowerLimit = t1-2*delta1,
+                                  upperLimit = t2+2*delta2,
+                                  t1 = t1,
+                                  t2 = t2,
+                                  scale1 = scale1,
+                                  delta1 = delta1,
+                                  scale2 = scale2,
+                                  delta2 = delta2)$integral
+
+posterior_density_prop <- function(t, t1, t2, scale1, delta1, scale2, delta2, const = NULL) {
   prop <- cubature::adaptIntegrate(f = likelihood,
                                    lowerLimit = t1-delta1,
                                    upperLimit = t,
                                    t1 = t1,
                                    t2 = t2,
-                                   shape1 = shape1,
+                                   scale1 = scale1,
                                    delta1 = delta1,
-                                   shape2 = shape2,
+                                   scale2 = scale2,
                                    delta2 = delta2)$integral/const
   return(prop)
 }
 
-posterior_tree <- function(t, t1, t2, shape1, delta1, shape2, delta2, p = 0.025) {
+posterior_tree <- function(t, t1, t2, scale1, delta1, scale2, delta2, const = NULL, p = 0.025) {
   prop <- posterior_density_prop(t = t,
                                    t1 = t1,
                                    t2 = t2,
-                                   shape1 = shape1,
+                                   scale1 = scale1,
                                    delta1 = delta1,
-                                   shape2 = shape2,
-                                   delta2 = delta2)
+                                   scale2 = scale2,
+                                   delta2 = delta2,
+                                 const = const)
   return(prop - p)
 }
 
-posterior_density_prop(t = 47, t1 = 0, t2 = 30, 
-               shape1 = shape1, delta1 = delta1, 
-               shape2 = shape2, delta2 = delta2)
-
-
-posterior_tree(t = 47, t1 = 0, t2 = 30, 
-                           shape1 = shape1, delta1 = delta1, 
-                           shape2 = shape2, delta2 = delta2,
-               p = 0.025)
-
-
-find_ci_limits <- function(t1, t2, shape1, delta1, shape2, delta2, alpha = 0.05) {
-  ci.lb <- uniroot(f = posterior_tree, lower = t1- 2 * delta1, upper = t2 + 2 * delta2, t1 = t1, t2 = t2, shape1 = shape1, delta1 = delta1, shape2 = shape2, delta2 = delta2, p = alpha/2)$root
-  ci.ub <- uniroot(f = posterior_tree, lower = t1- 2 * delta1, upper = t2 + 2 * delta2, t1 = t1, t2 = t2, shape1 = shape1, delta1 = delta1, shape2 = shape2, delta2 = delta2, p = 1 - alpha/2)$root
+find_ci_limits <- function(t1, t2, scale1, delta1, scale2, delta2, const = const, alpha = 0.05) {
+  ci.lb <- uniroot(f = posterior_tree, lower = t1- 2 * delta1, upper = t2 + 2 * delta2, t1 = t1, t2 = t2, scale1 = scale1, delta1 = delta1, scale2 = scale2, delta2 = delta2, const = const, p = alpha/2)$root
+  ci.ub <- uniroot(f = posterior_tree, lower = t1- 2 * delta1, upper = t2 + 2 * delta2, t1 = t1, t2 = t2, scale1 = scale1, delta1 = delta1, scale2 = scale2, delta2 = delta2, const = const, p = 1 - alpha/2)$root
   return(list(CI.LB = ci.lb, CI.UB = ci.ub))
 }
 
 
-find_ci_limits(t1, t2, shape1, delta1, shape2, delta2, alpha = 0.05)
+find_ci_limits(t1, t2, scale1, delta1, scale2, delta2, const = const, alpha = 0.05)
 
+
+
+
+
+# Testing stuff - ignore
+posterior_density_prop(t = 47, t1 = 0, t2 = 30, 
+                       scale1 = scale1, delta1 = delta1, 
+                       scale2 = scale2, delta2 = delta2, const = const)
+
+
+posterior_tree(t = 47, t1 = 0, t2 = 30, 
+               scale1 = scale1, delta1 = delta1, 
+               scale2 = scale2, delta2 = delta2,
+               const = const, p = 0.025)
+
+
+t1 <- 0
+d1 <- 15
+sigma1 <- 2
+delta1 <- min(3*sigma1,d1)
+scale1 <- uniroot(f = sigma_tree, lower = 1/(10*sigma1), upper = 5*(1/sigma1), sigma = sigma1, d = d1)$root
+print(scale1)
+test <- cubature::adaptIntegrate(f = likelihood,
+                                 lowerLimit = t1-delta1,
+                                 upperLimit = t1+delta1,
+                                 t1 = t1,
+                                 t2 = t2,
+                                 scale1 = scale1,
+                                 delta1 = delta1,
+                                 scale2 = scale2,
+                                 delta2 = delta2)$integral
+print(test)
+sigma1 <- 5
+delta1 <- min(3*sigma1,d1)
+scale1 <- uniroot(f = sigma_tree, lower = 1/(10*sigma1), upper = 5*(1/sigma1), sigma = sigma1, d = d1)$root
+print(scale1)
+test <- cubature::adaptIntegrate(f = likelihood,
+                                 lowerLimit = t1-delta1,
+                                 upperLimit = t1+delta1,
+                                 t1 = t1,
+                                 t2 = t2,
+                                 scale1 = scale1,
+                                 delta1 = delta1,
+                                 scale2 = scale2,
+                                 delta2 = delta2)$integral
+print(test)
